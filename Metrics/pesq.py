a6762e7a681452207872d14b946b86a4ce39c0c0
Ananyapam7 <ananyapam7@gmail.com>
2021-12-18 10:50:32 +0530

    Added PESQ metric

--- Metrics/pesq.py
+++ Metrics/pesq.py
@@ -3,7 +3,11 @@ from pypesq import pesq #install using pip install pypesq
 
 if __name__=="__main__":
     ref, sr = sf.read("../Testset/clean/sp01.wav") #input the clean speech sample
-    deg, sr = sf.read("../Testset/noisy/sp01_babble_sn10.wav") # input the noisy speech sample
     
+    deg, sr = sf.read("../Testset/noisy/sp01_babble_sn10.wav") # input the corresponding noisy speech sample
+    score = pesq(ref, deg, sr)
+    print(score)
+    
+    deg, sr = sf.read("../Testset/noisy/sp01_station_sn15.wav") 
     score = pesq(ref, deg, sr)
     print(score)

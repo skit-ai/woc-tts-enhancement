3fd062013167df0e98033a0113fbd5ba40aa64d8
Ananyapam7 <ananyapam7@gmail.com>
2021-12-19 19:57:01 +0530

    Update PESQ metric to torchaudio

--- Metrics/pesq_nb_wb.py
+++ Metrics/pesq_nb_wb.py
@@ -1,11 +1,14 @@
-import soundfile as sf
+import torchaudio
 from pesq import pesq #install using pip install pesq
 
 if __name__=="__main__":
     ## This implementation of PESQ supports both narrow band and wide band PESQ calculations
-    ref, sr = sf.read("../Testset/clean/sp01.wav") #input the clean speech sample
-    deg, sr = sf.read("../Testset/noisy/babble/sp01_babble_sn10.wav") # input the corresponding noisy speech sample
-    score = pesq(sr, ref, deg, "nb")
-    score = pesq(sr, ref, deg, "wb") #no wide band mode if fs = 8000
+    y_ref, sr = torchaudio.load("../Testset/clean/sp01.wav") #input the clean speech sample
+    y_syn, sr = torchaudio.load("../Testset/noisy/babble/sp01_babble_sn10.wav") # input the corresponding noisy speech sample
+
+    y_ref = y_ref.numpy()[0]
+    y_syn = y_syn.numpy()[0]
+    score = pesq(sr, y_ref, y_syn, "nb")
+    #score = pesq(sr, y_ref, y_syn, "wb") #no wide band mode if fs = 8000
     
     print(score)
